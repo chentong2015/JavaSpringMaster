35968f1872df424cc3760f33b7ee8c767a4a7878
tchen <tchen@murex.com>
2022-04-20 17:46:18 +0200

    add spring boot tester

---
+++ spring-boot-tester/src/main/java/com/spring/boot/MainSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainSpringBootApplication.class, args);
+    }
+}
---
+++ spring-boot-tester/src/main/java/com/spring/boot/MySpringBootConfiguration.java
@@ -0,0 +1,7 @@
+package com.spring.boot;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySpringBootConfiguration {
+}
