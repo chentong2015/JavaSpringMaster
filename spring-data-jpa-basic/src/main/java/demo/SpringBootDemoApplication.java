0c65a78a786bdab479b2129eeea736fd7ff062a9
tchen <tchen@murex.com>
2022-06-03 14:36:47 +0200

    enrich spring data jpa

--- spring-data-jpa-basic/src/main/java/demo/SpringBootDemoApplication.java
+++ spring-data-jpa-basic/src/main/java/demo/SpringBootDemoApplication.java
@@ -3,6 +3,8 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories
+// https://www.baeldung.com/spring-data-jpa-query#1-jpql-2
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
