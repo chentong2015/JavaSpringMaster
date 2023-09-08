8665cad401dcdb95702538ef38b6336eeeeedb92
tchen <tchen@murex.com>
2023-09-08 11:14:55 +0200

    update base spring framework

--- basic-spring-framework/spring-ioc-basic/src/main/java/spring_ioc/SpringIocBasic.java
+++ basic-spring-framework/spring-ioc-basic/src/main/java/spring_ioc/SpringIocBasic.java
@@ -6,20 +6,16 @@ import spring_ioc.bean.MyBean;
 // Spring能够更好的管理java中的对象，相当于对象的管理者 !!
 // 被Spring管理的对象才能完成自动注入，依赖注入
 
-// Spring IOC: 依赖注入 --> 控制反转
-// 1. 谁控制谁: IOC容器控制bean的声明周期
+// Spring IoC: 依赖注入 --> 控制反转
+// 1. 谁控制谁: IoC容器控制bean的声明周期
 // 2. 控制什么: Bean(对象)
 // 3. 为什么要控制: 解耦，依赖抽象
 // 4. 那些方面反转: 创建的权利反转，交给容器来托管
-
-// Spring 依赖查找:
-// UserController { Map<String, IRule> 使用接口 }
 public class SpringIocBasic {
 
     // 如何理解Spring IoC容器?  Spring IOC Container是什么?
-    // TODO: 完成一序列功能的一系列组件共同构成IOC容器，都在BeanFactory中
-    // BeanFactory IoC容器
-    // FactoryBean bean工厂: getObject() 从容器中返回的对象由这个对象确定
+    // BeanFactory IoC容器 : 完成一序列功能的一系列组件共同构成IOC容器
+    // FactoryBean bean工厂: getObject()从容器中返回的对象由这个对象确定
 
     // SingletonObjects  单例缓存池(存放bean对象的), 使用ConcurrentHashMap类型, 多个线程可以使用Spring中同一个单例缓存池
     // BeanDefinitionMap bean定义对象存储地方
