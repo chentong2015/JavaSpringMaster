44f1c54a7efd9ccef56347f46c5dc88bdb802b96
tchen <chen2015tong@gmail.com>
2024-10-08 15:14:21 +0200

    refresh spring resource loader

--- spring-context-beans/src/main/java/SpringResourceLoader.java
+++ spring-context-beans/src/main/java/SpringResourceLoader.java
@@ -3,28 +3,26 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 // TODO. Spring Core提供两种资源加载路径: Filesystem, Classpath
 public class SpringResourceLoader {
 
     public static void main(String[] args) throws IOException {
-        // TODO. 直接通过资源加载器加载classpath文件数据
-        ResourceLoader resourceLoader = new DefaultResourceLoader();
-        String resources = resourceLoader.getResource("static/sample.txt")
-                .getContentAsString(Charset.defaultCharset());
-        System.out.println(resources);
+        // TODO. 直接从Classpath获取Resource资源数据
+        Resource resource1 = new DefaultResourceLoader().getResource("static/sample.txt");
+        Resource resource2 = new ClassPathResource("static/app.json");
+        System.out.println(resource2.getContentAsString(Charset.defaultCharset()));
 
-
-        // 默认从项目根目录相对路径加载
+        // TODO. 直接从Filesystem文件系统路径加载Resource资源
         Resource fileSystemResource = new FileSystemResource("root/full/path/text.xml");
 
-        // 默认从/resources目录加载
-        Resource classPathResource = new ClassPathResource("static/app.json");
 
-        // 从classpath (/resources)目录加载
+        // 从classpath(/resources)目录加载FileInputStream
         File file = ResourceUtils.getFile("classpath:static/sample.txt");
         InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file));
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        System.out.println(bufferedReader.lines());
+
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        System.out.println(reader.lines().collect(Collectors.joining()));
     }
 }
