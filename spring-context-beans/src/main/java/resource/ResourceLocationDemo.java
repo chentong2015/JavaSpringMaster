27f4bcfd28846da23d1ca1249f213b4b2c73c303
tchen <chen2015tong@gmail.com>
2024-10-15 16:31:03 +0200

    add resource location demo

---
+++ spring-context-beans/src/main/java/resource/ResourceLocationDemo.java
@@ -0,0 +1,35 @@
+package resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@Component
+public class ResourceLocationDemo {
+
+    // Resource源资源来自于Classpath路径
+    @Value("file.csv")
+    private Resource resource1;
+
+    // File项目路径下的资源
+    @Value("file:spring-context-beans/src/main/resources/output.xml")
+    private Resource resource2;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    // TODO. 通过不同的定位方式获取到资源数据
+    public void testResourceLocations() throws IOException {
+        System.out.println(this.resource1.getContentAsString(Charset.defaultCharset()));
+
+        System.out.println(this.resource2.getContentAsString(Charset.defaultCharset()));
+
+        Resource resource3  = this.resourceLoader.getResource("classpath:file.txt");
+        System.out.println(resource3.getContentAsString(Charset.defaultCharset()));
+    }
+}
