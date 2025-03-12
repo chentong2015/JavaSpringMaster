7d83b6901701a4c14d115f0de836e1536d29ab6f
tchen <chen2015tong@gmail.com>
2025-03-12 17:02:27 +0100

    update spring resource loader

--- spring-context-beans/src/main/java/resource/ResourceLoaderCore.java
+++ spring-context-beans/src/main/java/resource/ResourceLoaderCore.java
@@ -7,24 +7,27 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.util.stream.Collectors;
 
-// TODO. Spring Core提供两种资源加载路径: Filesystem, Classpath
+// TODO. Spring提供两种资源加载路径: Filesystem, Classpath
 public class ResourceLoaderCore {
 
     public static void main(String[] args) throws IOException {
-        // TODO. 直接从Classpath获取Resource资源数据
+        // TODO. 从Classpath获取Resource资源数据
         Resource resource1 = new DefaultResourceLoader().getResource("static/sample.txt");
+
         Resource resource2 = new ClassPathResource("static/app.json");
+        FileInputStream file2 = new FileInputStream(resource2.getFile());
         System.out.println(resource2.getContentAsString(Charset.defaultCharset()));
         System.out.println(resource2.getURL());
 
-        // TODO. 直接从Filesystem文件系统路径加载Resource资源
-        Resource fileSystemResource = new FileSystemResource("root/full/path/text.xml");
 
-        // 从classpath(/resources)目录加载FileInputStream
+        // TODO. 从classpath(/resources)目录加载FileInputStream
         File file = ResourceUtils.getFile("classpath:static/sample.txt");
         InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file));
-
         BufferedReader reader = new BufferedReader(inputStreamReader);
         System.out.println(reader.lines().collect(Collectors.joining()));
+
+
+        // TODO. 从Filesystem文件系统路径加载Resource资源
+        Resource fileSystemResource = new FileSystemResource("root/full/path/text.xml");
     }
 }
